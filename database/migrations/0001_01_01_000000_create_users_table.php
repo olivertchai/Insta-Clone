947490c686b7672238b360b5b4dc2d13ea5700b1
Wagner Camargo Oliveira <32493077+olivertchai@users.noreply.github.com>
2026-04-28 21:51:34 -0300

    Refactor/migration user (#12)
    * Fixed the feature follows and images that does not load from post
    
    * Refactor the routes to unfollow
    
    * refactor the migration users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique()->after('name');
             $table->string('email')->unique();
+            $table->text('bio')->nullable()->after('email');
+            $table->string('avatar_url')->nullable()->after('bio');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
