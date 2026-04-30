158417ac4ca72891e7dea58d435466bb632402cb
Wagner Camargo Oliveira <32493077+olivertchai@users.noreply.github.com>
2026-04-29 21:07:50 -0300

    Refactor users table migration for clarity

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique()->after('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->text('bio')->nullable()->after('email');
-            $table->string('avatar_url')->nullable()->after('bio');
+            $table->text('bio')->nullable();
+            $table->string('avatar_url')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
